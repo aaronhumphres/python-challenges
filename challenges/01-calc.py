4a4f7e213a85fb4335e4d8493a32be07fbda1a1b
Aaron Humphres <aaronhumphres@yahoo.com>
2023-03-14 22:29:39 -0700

    completed assignment

--- challenges/01-calc.py
+++ challenges/01-calc.py
@@ -2,3 +2,23 @@
 # input() always returns a string value. If you ever want someone
 # to enter a number you have to use the `int()` function to convert
 # what they typed in to a string.
+
+def calculate():
+    prompt = input("What calculation would you like to do (add, sub, mul, div)? ")
+    num1 = int(input("Enter the first number:"))
+    num2 = int(input("Enter the second number:"))
+    result = 0
+    if prompt == "add":
+        result = num1 + num2
+        print(result)
+    elif prompt == "sub":
+        result = num1 - num2
+        print(result)
+    elif prompt == "mul":
+        result = num1 * num2
+        print(result)
+    elif prompt == "div":
+        result = num1 / num2
+        print(result)
+
+calculate()
--- challenges/04-alphabetical.py
+++ challenges/04-alphabetical.py
@@ -1,3 +1,10 @@
 # You'll need to use a couple of built in functions to alphabetize a string. 
 # Try to avoid looking up the exact answer and look at built in functions for
 # lists and strings instead.
+def sort(string):
+    alpha_string = list(string)
+    output = ''.join(sorted(alpha_string))
+    print(f'sorted: {output}')
+
+
+sort(string = input('anaconda:\n'))
